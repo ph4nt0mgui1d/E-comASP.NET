4f670e391e1c9569c87c773e7a99ca0d146a748b
ph4nt0mgui1d <aryansharma.bkn@gmail.com>
2023-04-27 11:50:55 +0530

    Initial commit

---
+++ E-Com_try1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Com_try1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
